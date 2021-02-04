1f4d49e68220c58f040336067042f83ac16d5f6e
Yunju Jang <rlo3olr@naver.com>
2021-02-04 16:32:06 +0900

    2021/02/04 프로그래머스 스택과 큐 3번 다리를 지나는 트럭

--- Stack&Queue/stack_queue/Bridge.java
+++ Stack&Queue/stack_queue/Bridge.java
@@ -1,60 +1,51 @@
-package stack_queue;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Bridge {
-	public static void main(String[] args) {
-		// 주어진 데이터
-		int bridge_length = 2;
-		int weight = 10;
-		int[] truck_weights = {7, 4, 5, 6};
-/*
-		int bridge_length = 100;
-		int weight = 100;
-		int[] truck_weights = {10};
-		*/
-		List<Integer> on_bridge = new ArrayList<Integer>();
-		int answer;
-		
-		int sec = 0;
-		int max = 0;
-		
-		for(int i = 0; i < truck_weights.length; i++) {
-
-			sec ++;
-			System.out.println(sec+"초");
-			if(sec >= weight && sec % bridge_length == 0) {
-				System.out.println(on_bridge.get(0) + "을 삭제");
-				max -= on_bridge.get(0);
-				on_bridge.remove(0);
-				sec++;
-				System.out.println("합계" + max);
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Truck> bridge = new LinkedList<Truck>();
+        int nextTruckIndex = 1;
+        int time = 1;
+        
+        int truckSum = truck_weights[0];
+		bridge.add(new Truck(truck_weights[0], 0));
+        
+        while(true) {
+			// 종료 조건
+			if(bridge.size() == 0) {
+				break;
 			}
-			System.out.println("현재 값 : " + truck_weights[i]);
-			if(max == 0) {
-				System.out.println("추가");
-				max += truck_weights[i];
-				System.out.println("max는 ! " + max);
-				on_bridge.add(truck_weights[i]);
-			} else if(max + truck_weights[i] <= 10) {
-				System.out.println("추가");
-				System.out.println("max값: " + max);
-				max += truck_weights[i];
-				on_bridge.add(truck_weights[i]);
-			} 
 			
-			if(i == truck_weights.length - 1) {
-				sec++;
+			time ++;
+			for(int i = 0; i < bridge.size(); i++) {
+				Truck truck = bridge.poll();
+				truck.position++;
+				
+				if(truck.position == bridge_length) {
+					truckSum -= truck.weight;
+					i--;
+				} else {
+					bridge.add(truck);
+				}
 			}
-			for(int on : on_bridge) {
-				System.out.println("잔여 : " + on);
-			}
-			System.out.println("-------");
 			
+			if(nextTruckIndex < truck_weights.length && truckSum + truck_weights[nextTruckIndex] <= weight) {
+				truckSum += truck_weights[nextTruckIndex];
+				bridge.add(new Truck(truck_weights[nextTruckIndex], 0));
+				nextTruckIndex ++;
+			}
 		}
-		
-
-		System.out.println(sec);
-	}
+        
+        return time;
+    }
+    
+    
+    class Truck{
+        int weight;
+        int position;
+        
+        public Truck(int weight, int position){
+            this.weight = weight;
+            this.position = position;
+        }
+    }
 }
