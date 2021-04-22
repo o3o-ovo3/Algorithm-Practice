02f1eea856d86d2422c7e57a88712d19a3ab33bd
Yunju Jang <rlo3olr@naver.com>
2021-04-22 20:46:09 +0900

    Update Phone_book.java

--- Hash/Phone_book.java
+++ Hash/Phone_book.java
@@ -1,25 +1,22 @@
 // 프로그래머스 - 해시 <전화번호 목록> 문제
-// 정확성 통과, 효율성 실패 (시간 초과)
+// startsWith로 문자열 비교
 
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
         Arrays.sort(phone_book);
 
-        for (int i = 0; i < phone_book.length; i++) {
-            String s = phone_book[i];
-            for (int j = i + 1; j < phone_book.length; j++) {
-                String str = phone_book[j];
-                if (s.length() <= str.length() && s.equals(str.substring(0, s.length()))) {
-                    answer = false;
-                    break;
-                }
-            }
-            if (!answer)
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])){
+                // 앞에서 정렬을 했기 때문에 양 옆만 비교
+                answer = false;
                 break;
+            }
         }
+        
         return answer;
     }
 }
