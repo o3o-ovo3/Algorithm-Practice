26340b8d8dc4c95ce492f9e0b8d5f93f8306fa55
Yunju Jang <rlo3olr@naver.com>
2021-06-14 15:24:55 +0900

    Update BiggestSquare.java

--- Level2/BiggestSquare.java
+++ Level2/BiggestSquare.java
@@ -1,65 +1,34 @@
 // 프로그래머스 레벨2 가장 큰 정사각형
-// 테스트케이스 통과, 채점 실패
+// 동적 프로그래밍 
 
-import java.util.LinkedList;
-import java.util.Queue;
 class Solution
 {
     public int solution(int [][]board)
     {
+        int[][] newBoard = new int[board.length + 1][board[0].length + 1];
+
         int answer = 0;
+        // 2X2 정사각형의 우측하단에서 시작
+        // 1이면 좌, 좌측 대각선, 상 방향으로 1이 있는지 확인
+        for(int r = 1; r < newBoard.length; r++)
+            for(int c = 1; c < newBoard[0].length; c++)
+                newBoard[r][c] = board[r - 1][c - 1];
 
-        Queue<Pair> q = new LinkedList<>();
-        int cMax = 0;
-        int max = 0;
-        boolean[][] visited = new boolean[board.length][board[0].length];
-       
-        // 전체 노드 탐색
-        for (int r = 0; r < board.length; r++) {
-            for (int c = 0; c < board[0].length; c++) {
-                if (visited[r][c])
-                    continue;
-                if (board[r][c] == 1) {
-                    q.add(new Pair(r, c));
-                }
-                while (!q.isEmpty()) {
-                    int cntC = 0; // 오른쪽 카운트
-                    int cntR = 0; // 아랫쪽 카운트
-                    Pair current = q.poll();
-
-                    // 오른쪽으로 쭉 탐색하고 아래로 한칸 내려가서 오른쪽으로 또 쭉 탐색
-                    for (int i = current.r; i < board.length; i++) {
-                        for (int j = current.c; j < board[0].length; j++) {
-                            if(visited[i][j])
-                                break;
-
-                            if (board[i][j] != 1)
-                                break;
-
-                            visited[i][j] = true;
-                            cntC++;
-                        }
-                        if(cMax == 0)
-                            cMax = cntC;
-                        else if(cntC!=0) cMax = Math.min(cntC, cMax);
-                        cntR++;
-                        cntC = 0;
-                    }
-                    int sub = Math.min(cntR, cMax);
-                    max = Math.max(max, sub);
+        int max = 0; // 가장 큰 정사각형 한 변의 길이
+        for(int i = 1; i < newBoard.length; i++){
+            for(int j = 1; j < newBoard[0].length; j++){
+                if(newBoard[i][j] == 1){
+                    int left = newBoard[i][j-1];
+                    int leftUp = newBoard[i-1][j-1];
+                    int up = newBoard[i-1][j];
+                    int min = Math.min(left, Math.min(leftUp, up));
+                    newBoard[i][j] = min + 1;
+                    max = Math.max(max, min+1);
                 }
             }
         }
+
         answer = max * max;
         return answer;
-    }
-}
-class Pair {
-    int r;
-    int c;
-
-    Pair(int r, int c) {
-        this.r = r;
-        this.c = c;
     }
 }
