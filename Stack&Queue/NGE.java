8972e57a1d3f2a54d59dd4d08b1f29bc266ce380
Yunju Jang <rlo3olr@naver.com>
2021-07-17 15:27:22 +0900

    Update NGE.java

--- Stack&Queue/NGE.java
+++ Stack&Queue/NGE.java
@@ -1,64 +1,52 @@
 // 백준 알고리즘 - 스택 오큰수
-// 시간 초과
 
 package baekjoon.stack;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class NGE {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
 
-        Stack<Integer> stack = new Stack<>();
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        for(int i = 0; i < N; i++)
-            stack.push(Integer.parseInt(st.nextToken()));
-
-        int[] arr = new int[N];
-        int max = stack.pop();
-        int peek = 0;
         int idx = N - 1;
-        arr[idx] = -1;
+        Stack<Integer> temp = new Stack<>();
+        Stack<Integer> answer = new Stack<>();
 
-        while(!stack.isEmpty()){
-            int now = stack.pop();
-            if(peek != 0){
-                if(now < peek){
-                    arr[--idx] = peek;
-                    max = peek;
-                }
-
-                else if(now < max) {
-                    arr[--idx] = max;
-                    peek = now;
-                }
-
-                else{
-                    arr[--idx] = -1;
-                    max = now;
-                }
+        while(idx >= 0){
+            if(temp.isEmpty()){
+                answer.push(-1);
+                temp.push(Integer.parseInt(input[idx]));
             }
+
             else{
-                if(now > max) {
-                    arr[--idx] = -1;
-                    max = now;
-                    peek = now;
-                }
-                else{
-                    arr[--idx] = max;
-                    peek = now;
+                while(true){
+                    if(temp.isEmpty()){
+                        temp.push(Integer.parseInt(input[idx]));
+                        answer.push(-1);
+                        break;
+                    }
+                    if(Integer.parseInt(input[idx]) >= temp.peek())
+                        temp.pop();
+                    else{
+                        answer.push(temp.peek());
+                        temp.push(Integer.parseInt(input[idx]));
+                        break;
+                    }
                 }
             }
+            idx --;
         }
-        for (int i = 0; i < N; i++){
-            System.out.print(arr[i]);
-            if(i != N-1)
-                System.out.print(" ");
+
+        while(!answer.isEmpty()){
+            sb.append(answer.pop());
+            if(!answer.isEmpty())
+                sb.append(" ");
         }
+        System.out.println(sb);
     }
 }
