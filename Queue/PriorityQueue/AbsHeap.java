51ace413606d7c13de07217e791ed3d6c7b842aa
Yunju Jang <rlo3olr@naver.com>
2021-07-30 01:22:38 +0900

    Update AbsHeap.java

--- Queue/PriorityQueue/AbsHeap.java
+++ Queue/PriorityQueue/AbsHeap.java
@@ -1,46 +1,48 @@
 // 백준 알고리즘 우선순위 큐 - 절댓값 힙
-// 시간초과
 
-import java.util.ArrayList;
-import java.util.List;
+package baekjoon.queue;
+
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class Main {
+public class AbsHeap {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         int N = sc.nextInt();
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        List<String> list = new ArrayList<>();
+        PriorityQueue<Integer> plus = new PriorityQueue<>(); // 양수 큐
+        PriorityQueue<Integer> minus = new PriorityQueue<>(); // 음수 큐
 
         while(N > 0){
             int x = sc.nextInt();
-            if(x == 0){
-                if(pq.isEmpty())
+
+            // 삽입 연산
+            if(x < 0) // 음수일 때
+                minus.add(x * -1);
+
+            else if(x > 0)// 양수일 때
+                plus.add(x);
+
+            // 0일 때 --> 삭제 연산
+            else{
+                if(plus.isEmpty() && minus.isEmpty()) // 큐에 아무것도 없으면 0
                     sb.append("0\n");
-                else{
-                    int value = pq.peek();
-                    String strVal = "";
-                    if(list.contains(Integer.toString(value * -1))) {
-                        sb.append(pq.poll() * -1 + "\n");
-                        strVal = Integer.toString(value *= -1);
-                        list.remove(strVal);
-                    }
-                    else{
-                        sb.append(pq.poll() + "\n");
-                        strVal = Integer.toString(value);
-                        list.remove(strVal);
-                    }
+
+                else if(!plus.isEmpty() && minus.isEmpty())
+                    sb.append(plus.poll() + "\n"); // 양수 큐에만 있을 때 가장 작은 애를 뺌
+
+                else if(plus.isEmpty() && !minus.isEmpty())
+                    sb.append(minus.poll() * -1 + "\n"); // 음수 큐에만 있으면 절대값이 가장 작은 애를 빼서 -1을 곱함
+
+                else{ // 둘 다 큐에 원소가 있고
+                    if(plus.peek() >= minus.peek()) // 같아도 마이너스를 뽑는거니까
+                        sb.append(minus.poll() * -1 + "\n");
+
+                    else
+                        sb.append(plus.poll() + "\n");
                 }
             }
-
-            else{
-                list.add(Integer.toString(x));
-                pq.add(Math.abs(x));
-            }
-
             N--;
         }
         System.out.println(sb);
